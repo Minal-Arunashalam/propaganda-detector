9060956b996160aa29f108123e115a52b04e15de
Minal-Arunashalam <minal.shalam@gmail.com>
2025-11-30 15:41:16 -0500

    removed another dupetraining folder

--- training/configs.py
+++ training/configs.py
@@ -2,9 +2,9 @@ class Config:
     model_name = "roberta-base"
     num_labels = 14
 
-    train_csv = "processed/train.csv"
-    val_csv = "processed/val.csv"
-    test_csv = "processed/test.csv"
+    train_csv = "../data/processed/train.csv"
+    val_csv = "../data/processed/val.csv"
+    test_csv = "../data/processed/test.csv"
 
     max_len = 256
     batch_size = 8
