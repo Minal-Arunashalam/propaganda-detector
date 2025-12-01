899622d292e831aeac1c46cc7d7f202c205d8b28
Minal <minal.shalam@gmail.com>
2025-12-01 13:25:29 -0500

    Update model name in LlamaConfig class

--- training/configs.py
+++ training/configs.py
@@ -29,7 +29,7 @@ class DebertaConfig:
 
 
 class LlamaConfig:
-    model_name = "meta-llama/Llama-3.1-8b-hf"  # Latest free Llama model
+    model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     num_labels = 14
 
     train_csv = "data/processed/train.csv"
